cc9a5bd5badb6dc1676b36ed55905498390e297d
Amandeep Singh <133962791+amandeep-dhiman@users.noreply.github.com>
2025-04-22 13:17:23 +0530

    feat: integrate Supabase for authentication and session management

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -12,6 +12,7 @@ import FacebookIcon from "@images/icons/FacebookIcon";
 import DiscordIcon from "@images/icons/DiscordIcon";
 import GoogleIcon from "@images/icons/GoogleIcon";
 
+import { createClient } from "@/utils/supabase/client";
 import { Separator } from "@/components/separator";
 
 interface LoginProps {}
@@ -43,8 +44,16 @@ const Login: FunctionComponent<LoginProps> = () => {
 
   const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+  };
 
-    alert("Form submitted!");
+  const supabase = createClient();
+  const handleSocialLogin = (provider: "google" | "linkedin_oidc") => {
+    supabase.auth.signInWithOAuth({
+      provider,
+      options: {
+        redirectTo: `http://localhost:3000/auth/callback`
+      }
+    });
   };
 
   return (
@@ -182,6 +191,7 @@ const Login: FunctionComponent<LoginProps> = () => {
               color="default"
               size="lg"
               variant="bordered"
+              onPress={() => handleSocialLogin("google")}
             >
               <GoogleIcon />
             </Button>
